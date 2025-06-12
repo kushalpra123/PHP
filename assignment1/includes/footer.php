b1da8767df6ccc4414d374896be4e0e5be90a65d
kushalpra123 <kushalpra49@gmail.com>
2025-06-12 13:50:47 -0400

    Delete assignment1 directory

--- assignment1/includes/footer.php
+++
@@ -1,9 +0,0 @@
-    </main>
-    <footer class="bg-dark text-white text-center py-3 mt-4">
-        &copy; <?php echo date("Y"); ?> Student Portal
-    </footer>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
--- assignment1/index.php
+++
@@ -1,9 +0,0 @@
-<?php include_once 'includes/header.php'; ?>
-<div class="jumbotron">
-    <h1 class="display-4">Welcome to the Student Portal</h1>
-    <p class="lead">Add and view student records with ease.</p>
-    <hr class="my-4">
-    <a class="btn btn-primary btn-lg" href="add.php" role="button">Add Student</a>
-    <a class="btn btn-secondary btn-lg" href="view.php" role="button">View Students</a>
-</div>
-<?php include 'includes/footer.php'; ?>
